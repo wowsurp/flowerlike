bb3456060684e0f87263ceb4ff6c54365e46bec9
jes89 <hiter00@naver.com>
2019-09-09 16:41:21 +0900

    리플렉션 구

--- src/main/java/com/ajs/apppush/mutation/UserMutation.java
+++ src/main/java/com/ajs/apppush/mutation/UserMutation.java
@@ -1,6 +1,10 @@
 package com.ajs.apppush.mutation;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -8,6 +12,7 @@ import org.springframework.stereotype.Component;
 import com.ajs.apppush.entity.User;
 import com.ajs.apppush.input.UserInput.SaveUserInput;
 import com.ajs.apppush.repository.UserRepository;
+import com.ajs.apppush.util.ReflectionUtil;
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 
 @Component
@@ -15,16 +20,23 @@ public class UserMutation implements GraphQLMutationResolver{
 
 	@Autowired
 	private UserRepository userRepository;
+		
+	@Autowired
+	private ReflectionUtil reflectionUtil;
 	
 	public UserMutation(UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
 	
-	public User saveUser(SaveUserInput userInput) {
+	public User saveUser(SaveUserInput userInput) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		
 		User user = null;
 		
 		Optional<User> opUser = userRepository.findById(userInput.getUserId());
+		Properties prop = new Properties();
+		
+		reflectionUtil.overWriteProperties(opUser, userInput);
+		
 		
 		if(opUser.isPresent()) {
 			
---
+++ src/main/java/com/ajs/apppush/util/ReflectionUtil.java
@@ -0,0 +1,53 @@
+package com.ajs.apppush.util;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReflectionUtil {
+
+	final String GETTER_PREFIX = "GET";
+	final String SETTER_PREFIX = "SET";
+	
+	public Object overWriteProperties(Object originObj, Object newObj) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+
+		Class newObjClass = newObj.getClass();
+
+		Field[] newObjFiledsArr = newObjClass.getDeclaredFields();
+
+		Method[] newObjMethodArr = newObjClass.getMethods();
+
+		for (Field tempField : newObjFiledsArr) {
+			for (Method tempMethod : newObjMethodArr) {
+				String methodNm = tempMethod.getName().toUpperCase();
+				String fieldNm = tempField.getName().toUpperCase();
+
+				if (methodNm.equals(GETTER_PREFIX + fieldNm)) {
+					this.callSetters(fieldNm, originObj, tempMethod.invoke(newObj));
+				}
+			}
+		}
+
+		return originObj;
+	}
+
+	private void callSetters(String targetFieldNm, Object originObj, Object value) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+
+		Class originObjClass = originObj.getClass();
+
+		Method[] originObjMethodArr = originObjClass.getMethods();
+
+		for (Method tempMethod : originObjMethodArr) {
+			
+			String methodNm = tempMethod.getName().toUpperCase();
+
+			if (methodNm.equals(SETTER_PREFIX + targetFieldNm)) {
+				tempMethod.invoke(originObj, value);
+			}
+		}
+		
+	}
+}
