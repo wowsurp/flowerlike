74e1c1745f25a4ca9bd78af5544b0e57276961b8
jes <hiter00@naver.com>
2019-09-11 10:49:01 +0900

    커커커커 커밋

--- src/main/java/com/ajs/apppush/jwt/JwtTokenFilter.java
+++ src/main/java/com/ajs/apppush/jwt/JwtTokenFilter.java
@@ -1,17 +1,20 @@
 package com.ajs.apppush.jwt;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.ContentCachingRequestWrapper;
 
 @Configuration
 public class JwtTokenFilter extends OncePerRequestFilter{
@@ -24,19 +27,39 @@ public class JwtTokenFilter extends OncePerRequestFilter{
 		
 		logger.info("doFilterInternal : ");
 		
-		ContentCachingRequestWrapper reqWrapper = new ContentCachingRequestWrapper(req);
+		AuthenticationRequestWrapper test = new AuthenticationRequestWrapper(req);
 		
-		String requestBody = new String(reqWrapper.getContentAsByteArray());
-		
-		logger.info("body : " + requestBody);
-		
-		filterChain.doFilter(reqWrapper, res);
-		
-//		String body = reqWrapper.getReader().lines()
-//				.reduce("", String::concat);
-		
-		
-		
+		String body = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+ 
+        try {
+            InputStream inputStream = test.getInputStream();
+            if (inputStream != null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                char[] charBuffer = new char[128];
+                int bytesRead = -1;
+                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            } else {
+                stringBuilder.append("");
+            }
+        } catch (IOException ex) {
+            throw ex;
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException ex) {
+                    throw ex;
+                }
+            }
+        }
+ 
+        body = stringBuilder.toString();
+        
+		filterChain.doFilter(test, res);
 		
 		
 	}
